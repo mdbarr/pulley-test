861a6000c1ba15d165bed9b8c7c61777524e54da
Mark Barr <mark@hyperingenuity.com>
2018-05-21 18:04:15 -0400

    Remove interative and timings as they showed no measurable difference.

--- index.js
+++ index.js
@@ -2,37 +2,13 @@
 'use strict';
 
 function factorial(number) {
-  if (number <= 1) {
-    return 1;
-  } else {
-    return number * factorial(number - 1);
-  }
-}
-
-function factorial_i(number) {
-  let factorial = 1;
+  let result = 1;
   for (let i = 2; i <= number; i++) {
-    factorial *= i;
+    result *= i;
   }
-  return factorial;
+  return result;
 }
 
-function stopwatch(func, value) {
-  const start = Date.now();
-
-  const result = func.apply(this, [ value ]);
-
-  const time = Date.now() - start;
-
-  console.log(time, start, Date.now());
-
-  return [ result, time ];
-}
-
-for (let i = 1; i <= 2000; i++) {
-  const [ resultR, timeR ] = stopwatch(factorial, i);
-  const [ resultI, timeI ] = stopwatch(factorial, i);
-
-  console.log(`Factorial   ${ i } = ${ resultR } (${ timeR }ms)`);
-  console.log(`Factorial_i ${ i } = ${ resultI } (${ timeI }ms)`);
+for (let i = 1; i <= 150; i++) {
+  console.log(`Factorial ${ i } = ${ factorial(i) }`);
 }
