e5516b020476d8cd8ac111e7bd28afa59f7a09c6
Mark Barr <mark@hyperingenuity.com>
2018-05-21 17:50:30 -0400

    Simple, recursive factorial generator.

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 #!/usr/bin/env node
 'use strict';
 
-console.log('Hello, world!');
+function factorial(number) {
+  if (number <= 1) {
+    return 1;
+  } else {
+    return number * factorial(number - 1);
+  }
+}
+
+for (let i = 1; i <= 20; i++) {
+  console.log(`Factorial ${ i } = ${ factorial(i) }`);
+}
