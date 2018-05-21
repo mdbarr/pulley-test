fcdb04f6d98cd818563dee78beb2de801f3c8580
Mark Barr <mark@hyperingenuity.com>
2018-05-21 17:52:35 -0400

    Interative factorial calculator.

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ function factorial(number) {
   }
 }
 
+function factorial_i(number) {
+  let factorial = 1;
+  for (let i = 2; i <= number; i++) {
+    factorial *= i;
+  }
+  return factorial;
+}
+
 for (let i = 1; i <= 20; i++) {
-  console.log(`Factorial ${ i } = ${ factorial(i) }`);
+  console.log(`Factorial   ${ i } = ${ factorial(i) }`);
+  console.log(`Factorial_i ${ i } = ${ factorial_i(i) }`);
 }
